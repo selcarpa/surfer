f12ba5dd8a584cd645f6399c92e27fab4a0cdc85
selcarpa <selcarpa@gmail.com>
2025-02-26 16:31:43 +0800

    fix log level

--- src/main/kotlin/stream/Surfer.kt
+++ src/main/kotlin/stream/Surfer.kt
@@ -251,7 +251,7 @@ private fun connect(
 class SurferInitializer(private val handlerPairs: (Channel) -> MutableList<HandlerPair>) :
     ChannelInitializer<Channel>() {
     override fun initChannel(ch: Channel) {
-        ch.pipeline().addFirst(LOG_HANDLER, LoggingHandler(LogLevel.INFO, ByteBufFormat.HEX_DUMP))
+        ch.pipeline().addFirst(LOG_HANDLER, LoggingHandler(LogLevel.TRACE, ByteBufFormat.HEX_DUMP))
         ch.pipeline().addFirst(GLOBAL_TRAFFIC_SHAPING, ProxyChannelInitializer.globalTrafficShapingHandler)
         //todo: set idle timeout, and close channel
         ch.pipeline().addFirst(IDLE_CLOSE_HANDLER, IdleCloseHandler())
